d35473708287bcf824e0741addd76c135f8aa2cb
JoshuaMartine <popopotro141@gmail.com>
2024-01-19 12:27:37 -0400

    cambios del menu

--- home/yes.js
+++ home/yes.js
@@ -1,35 +1,27 @@
 // Función para mostrar la alerta y redirigir a otro archivo después de un tiempo
 function alertaYRedireccion() {
-          // Mostrar la alerta
-          alert('Solo usuarios autorizados');
+  // Mostrar la alerta
+  alert('Solo usuarios autorizados');
 
-          setTimeout(function() {
-            window.location.href = '../login/formulario.html';
-          }, 100); 
-        }
-      
-        const botonIr = document.getElementById('irAOtroArchivo');
-      
-        botonIr.addEventListener('click', function() {
-          alertaYRedireccion();
-        });
-
-        
-let hideText_btn = document.getElementById('hideText_btn');
-
-let hideText = document.getElementById('hideText');
-        
-
-hideText_btn.addEventListener('click', toggleText);
-
-function toggleText(){
-  hideText.classList.toggle('show');
-
-  if(hideText.classList.contains('show')){
-    hideText_btn.innerHTML = 'leer menos';
-  }
-
-  else{
-    hideText_btn.innerHTML = 'leer mas';
-  }
+  setTimeout(function() {
+    window.location.href = '../login/formulario.html';
+  }, 100); 
 }
+
+const botonIr = document.getElementById('uploadInfo');
+
+botonIr.addEventListener('click', function() {
+  alertaYRedireccion();
+});
+
+
+document.getElementById("enlaceArchivo").addEventListener("click", function() {
+  // Cambiar la ubicación a tu archivo deseado (por ejemplo, "otras_carpetas/archivo_deseado.html")
+  window.location.href = "egresados/egresados.html";
+});
+
+
+document.getElementById("Inicio").addEventListener("click", function() {
+  // Cambiar la ubicación a tu archivo deseado (por ejemplo, "otras_carpetas/archivo_deseado.html")
+  window.location.href = "index.html";
+        });
