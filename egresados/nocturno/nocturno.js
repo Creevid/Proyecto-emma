6477528ebcaf727b2123c46f13dbfddf1bc39d5a
Gertrudio <jostinbautista20.10.2006@gmail.com>
2024-01-22 10:03:28 -0400

    Funcionalidad a los botones de subir informacion en las secciones de egresados

--- egresados/nocturno/nocturno.js
+++ egresados/nocturno/nocturno.js
@@ -1,6 +1,23 @@
-window.onload = function() {
+window.onload = function () {
     var dropdown = document.querySelector('.menuVertical');
-    dropdown.addEventListener('click', function() {
+    dropdown.addEventListener('click', function () {
         dropdown.classList.toggle('show');
     });
-  }
+}
+
+
+// Función para mostrar la alerta y redirigir a otro archivo después de un tiempo
+function alertaYRedireccion() {
+    // Mostrar la alerta
+    alert('Solo usuarios autorizados');
+
+    setTimeout(function () {
+        window.location.href = "/login/formulario.html";
+    }, 100);
+}
+
+const botonIr = document.getElementById('uploadInfo');
+
+botonIr.addEventListener('click', function () {
+    alertaYRedireccion();
+});
